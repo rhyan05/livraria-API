a084bbae52cc8fd0a774aa30b1441c73e284b283
rhyan05 <rhyanlemos05@gmail.com>
2024-09-23 22:21:20 -0300

    Primeira versao da api crud livraria

---
+++ db.php
@@ -0,0 +1,20 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $db_name = "livraria";
+    private $username = "seu_usuario";
+    private $password = "sua_senha";
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("pgsql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $exception) {
+            echo "Erro de conexão: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
+}
+?>
